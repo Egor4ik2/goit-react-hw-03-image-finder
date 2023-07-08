6ced09b59092b5ff7b9c6c1ef8af473c0cba37be
Egor Krechuniak <egorkrecunak@gmail.com>
2023-07-08 10:27:01 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import ImageGallery from './ImageGal/ImageGallery';
 import Button from './Button/Button';
 import Loader from './Loader/Loader';
 import Modal from './Modal/Modal';
-import { fetchImages } from './api/Api';
+import { fetchImages } from './api/Api'; 
 import styles from './App.module.css';
 
 export class App extends Component {
@@ -28,13 +28,16 @@ export class App extends Component {
 
     this.setState({ isLoading: true });
 
-    fetchImages(searchQuery, page)
+    fetchImages(searchQuery, page) 
       .then((hits) => {
-        this.setState((prevState) => ({
-          images: [...prevState.images, ...hits],
-          page: prevState.page + 1,
-          hasMoreImages: hits.length > 0, 
-        }));
+        if (hits.length > 0) {
+          this.setState((prevState) => ({
+            images: [...prevState.images, ...hits],
+            hasMoreImages: hits.length === 12,
+          }));
+        } else {
+          this.setState({ hasMoreImages: false });
+        }
       })
       .catch((error) => console.log(error))
       .finally(() => {
@@ -54,6 +57,14 @@ export class App extends Component {
     this.setState({ selectedImageURL: '' });
   };
 
+  handleLoadMore = () => {
+    this.setState((prevState) => ({
+      page: prevState.page + 1,
+    }), () => {
+      this.fetchImages();
+    });
+  };
+
   render() {
     const { images, isLoading, selectedImageURL, hasMoreImages } = this.state;
 
@@ -65,7 +76,9 @@ export class App extends Component {
 
         {isLoading && <Loader />}
 
-        {hasMoreImages && images.length > 0 && !isLoading && <Button onClick={this.fetchImages} />}
+        {images.length > 0 && hasMoreImages && (
+          <Button onClick={this.handleLoadMore} />
+        )}
 
         {selectedImageURL && (
           <Modal imageURL={selectedImageURL} onCloseModal={this.handleCloseModal} />
@@ -74,4 +87,3 @@ export class App extends Component {
     );
   }
 }
-
